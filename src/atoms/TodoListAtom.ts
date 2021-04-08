625fd39f7e5eaf709c5e08bd878421d5b43d144c
mimura <musyu36@gmail.com>
2021-04-08 15:54:12 +0900

    feat: tTodoList TodoTitleForm SearchTextForm atomの定義

---
+++ src/atoms/TodoListAtom.ts
@@ -0,0 +1,7 @@
+import { atom } from "recoil";
+import Todo from "../types/Todo";
+
+export const todoListState = atom<Todo[]>({
+  key: "todoList",
+  default: [{ title: "one" }, { title: "two" }, { title: "three" }],
+});
